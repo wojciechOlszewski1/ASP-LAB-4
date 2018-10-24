9a724cbf8849135c27ec57427de531eb2898074e
Wojciech <wojciech.olszewski3@gmail.com>
2018-10-24 21:33:16 +0200

    fb auth

--- Lab4Auth/Models/AccountViewModels.cs
+++ Lab4Auth/Models/AccountViewModels.cs
@@ -8,6 +8,7 @@ namespace Lab4Auth.Models
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
+        public int Year { get; set; }
     }
 
     public class ExternalLoginListViewModel

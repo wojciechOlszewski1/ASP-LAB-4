b8b894e2b8d394b6bb32469a373a443725503f37
Wojciech <wojciech.olszewski3@gmail.com>
2018-10-24 19:57:07 +0200

    add own method and views, add first migrawion

--- Lab4Auth/Controllers/HomeController.cs
+++ Lab4Auth/Controllers/HomeController.cs
@@ -1,30 +1,88 @@
-﻿using System;
+﻿using Lab4Auth.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using Lab4Auth.ViewModels;
 
 namespace Lab4Auth.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _db; //
+
+        public HomeController()
+        {
+            _db = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing) // niszczenie obiektu 
+        {
+            _db.Dispose();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return View(_db.Customers.Include(x => x.City).ToList());
         }
 
-        public ActionResult About()
+        public ActionResult CreateCustomer() // formularz dla nowego użytkownika
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var cities = _db.Cities.ToList();
+            CustomersWithCityViewModel customersWithCityView = new CustomersWithCityViewModel()
+            {
+                Cities = cities,
+                Customer = new Customer()
+            };
+            return View("CustomerForm", customersWithCityView);
         }
 
-        public ActionResult Contact()
+        public ActionResult EditCustomer(int id) // edycja istniejącego użytkownika
         {
-            ViewBag.Message = "Your contact page.";
+            var customer = _db.Customers.SingleOrDefault(x => x.Id == id);
+            if (customer == null)
+                return HttpNotFound();
 
-            return View();
+            CustomersWithCityViewModel customersWithCityViewModel = new CustomersWithCityViewModel()
+            {
+                Cities = _db.Cities.ToList(),
+                Customer = customer
+            };
+
+            return View("CustomerForm", customersWithCityViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer) // dodawanie do bazy danych
+        {
+            if (ModelState.IsValid)
+            {
+                if (customer.Id == 0)
+                {
+                    _db.Customers.Add(customer);
+                    _db.SaveChanges();
+                }
+                else
+                {
+                    var customerToUpdate = _db.Customers.Single(x => x.Id == customer.Id);
+                    customerToUpdate.Name = customer.Name;
+                    customerToUpdate.Surname = customer.Surname;
+                    customerToUpdate.Phone = customer.Phone;
+                    _db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var cities = _db.Cities.ToList();
+                CustomersWithCityViewModel customersWithCityView = new CustomersWithCityViewModel()
+                {
+                    Customer = customer,
+                    Cities = cities
+                };
+                return View("CustomerForm", customersWithCityView);
+            }
         }
     }
 }

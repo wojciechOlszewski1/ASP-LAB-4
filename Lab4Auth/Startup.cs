c15047867c5dd51c34cc1d7fc2dc6278b118205b
Wojciech <wojciech.olszewski3@gmail.com>
2018-10-24 19:29:56 +0200

    start

---
+++ Lab4Auth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab4Auth.Startup))]
+namespace Lab4Auth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
